fe637b9d1a8c3ba95b4cd9e52b6ee415c77bae0f
iqbigbang <iqbigbang@gmail.com>
2020-05-20 17:14:30 +0200

    docs(stdlib): improve documentation for std.io and std.fmt modules

--- std/fmt.h
+++ std/fmt.h
@@ -1,8 +1,14 @@
 #ifndef STD_FMT_H
 #define STD_FMT_H
-
 #include "core.h"
 
+/* std.fmt module
+ * Contains functions for working with strings
+ * and formatting or converting them
+ */
+
+/* Converts an integer to its string representation
+ */
 string_t* _Mitoa_I0(int n);
 
 #endif
